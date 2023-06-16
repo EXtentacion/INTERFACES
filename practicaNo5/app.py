dc1d8e56b89ac3a579c13367cc66f5a280e38496
EXtentacion <125932096+EXtentacion@users.noreply.github.com>
2023-06-15 18:41:54 -0600

    rutas y variables de conexion

--- practicaNo5/app.py
+++ practicaNo5/app.py
@@ -1,14 +1,39 @@
-
+#instanciamos la clase Flask
 from flask import Flask
 
+#inicializamos la app
 app = Flask(__name__)
+app.config ['MySQL_DATABASE_HOST'] = 'localhost'
+app.config ['MySQL_DATABASE_USER'] = 'root'
+app.config ['MySQL_DATABASE_PASSWORD'] = ''
+app.config ['MySQL_DATABASE_DB'] = 'dbflask'
 
+
+
+
+
+
+#Definimos la ruta  
 @app.route('/')
 
+#creamos la funcion
 def index():
-    
     return 'Hola mundo'
 
+@app.route('/guardar')
+def guardar():
+    return 'Guardado'
+
+@app.route('/eliminar')
+def eliminar():
+    
+    return 'Eliminado'
+
+
+
+
+
+#creamos la ruta
 if __name__ == '__main__':
     
     app.run(debug = True, port = 8000)
